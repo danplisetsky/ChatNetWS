11cdc44b2fd32077109b6ea924e0e5f44736220b
Daniel Gotshtein <gjiamavc@gmail.com>
2017-09-20 16:42:41 +0300

    Add TCP Server

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatNet
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            TcpHelper.StartServer(8000);
+            TcpHelper.Listen();
+        }
+    }
+}
